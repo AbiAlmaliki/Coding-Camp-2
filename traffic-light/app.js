a82887756bb9c789ae0eccab9e28cab7c556df9c
AbiAlmaliki <89960632+AbiAlmaliki@users.noreply.github.com>
2026-05-13 21:44:52 +0700

    Traffic light simmulator

---
+++ traffic-light/app.js
@@ -0,0 +1,36 @@
+// get variable
+const redLight = document.getElementById("red");
+const yellowLight = document.getElementById("yellow");
+const greenLight = document.getElementById("green");
+const switchBtn = document.getElementById("switchBtn");
+
+// initial value
+redLight.style.background = "red";
+let current = "red";
+
+// reset light
+function resetLight (){
+        redLight.style.background = "#444";
+        yellowLight.style.background = "#444";
+        greenLight.style.background = "#444";
+
+}
+// switch light
+function switchLight () {
+    resetLight ();
+    yellowLight.style.background = "yellow";
+
+    console.log(current);
+    setTimeout (() => {
+        resetLight();
+        if (current == "red") {
+            current = "green";
+            greenLight.style.background = "green";
+     } else {
+            current = "red";
+            redLight.style.background = "red"
+     }
+    }, 1000);
+}
+
+switchBtn.addEventListener("click", switchLight);
